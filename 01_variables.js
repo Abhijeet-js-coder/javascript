963d61f3ab7fb242aaae6111e588731649eb4ec2
Abhijeet-js-coder <abhijeetd887@gmail.com>
2025-06-24 11:44:21 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 142
+let accountEmail = "abhijeet@gmail.com"
+var accountPassword = "1780"
+accountCity = "Banglore"
+let accountState;
+
+// accountId= 2 not allowed
+
+accountEmail="dsha@gmail.com"
+accountPassword=6000
+accountCity="Mumbai"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefering not to use var when declaring a variable because of issue in block scope and functional scope
+*/
